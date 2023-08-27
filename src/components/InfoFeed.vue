e9ccfd4feda7e59de1db9904e847c954b7fa8b67
ikotun-dev <danlogan2003@gmail.com>
2023-08-27 21:56:47 +0100

    gallery dashboard

--- src/components/InfoFeed.vue
+++ src/components/InfoFeed.vue
@@ -1,8 +1,16 @@
 <template>
     <TopHeader/>
     <BaseHeader/>
-    <div class="h-64 w-90 border border-green-950 mx-2 my-2 rounded-lg ">
-            
+    <div class="ml-4 flex mt-2 items-center">
+    <router-link to="/find-employees" class="text-3xl mr-2 font-bold text-green-900">
+      &leftarrow;
+    </router-link>
+    </div>
+    <div class="h-48 w-90 border border-green-950 mx-2 my-2 rounded-lg lg:hidden">
+        <img class="h-48 rounded-lg" :src="require('../assets/images/slider4.jpg')"/>
+    </div>
+    <div class="h-48 w-90 border border-green-950 mx-2 my-2 rounded-lg lg:hidden">
+        <img class="h-48 rounded-lg" :src="require('../assets/images/slider3.jpg')"/>
     </div>
 </template>
 <script>
