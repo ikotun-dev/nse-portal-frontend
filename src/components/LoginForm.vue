7d5d9d79b84bdc93e106c1f33cda66c490b2801a
ikotun-dev <danlogan2003@gmail.com>
2023-09-20 19:02:42 +0100

    changes

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,11 +1,12 @@
 <template>
+ 
   <div class="flex-col" :src="require('../assets/images/slider4.jpg')">
     <div class="bg-image min-h-200 flex justify-center items-center">
       <SignupForm v-show="showSignup" @showLoginForm="showSignUp"></SignupForm>
       <div class="flex justify-center pt-3 lg:pt-12 pb-72 " v-show="showLogin">
       <form>
-        <div class="bg-white p-2 border rounded-md">
-          <div class="flex flex-col px-4 py-4 items-center w-86 lg:w-100 h-64 pt-14 border border-orange-900 rounded-md">
+        <div class="bg-white p-2 pb-2 border rounded-md">
+          <div class="flex flex-col px-4 py-4 items-center w-86 lg:w-100 h-72 pt-14 border border-orange-900 rounded-md pb-8">
             <label class="font-serif text-lg lg:text-2xl mb-2 text-green-800"><b>NSE-Oluyole</b> Member Login</label>
             <h3 class="text-red-800 font-pop text-sm" v-show="wrongCredentials">Engineer Match Not Found</h3>
             <div
@@ -14,6 +15,12 @@
                 class="px-2 py-2 h-8 lg:w-64 w-64 rounded-sm sm:w-56 focus:outline-none text-xs focus:ring-10 font-pop focus:ring-green-800 ring-1 ring-green-600 "
                 type="text" placeholder="NSE-NO :" v-model="username" required />
             </div>
+            <div
+              class="h-8 lg:w-64 rounded-sm sm:w-56 focus:outline-none text-xs focus:ring-10 font-pop focus:ring-green-950 ring-1 ring-green-800 mt-2">
+              <input
+                class="px-2 py-2 h-8 lg:w-64 w-64 rounded-sm sm:w-56 focus:outline-none text-xs focus:ring-10 font-pop focus:ring-green-800 ring-1 ring-green-600 "
+                type="text" placeholder="PASSWORD :" v-model="password" required />
+            </div>
 
             <button
               class="font-pop border py-2 px-16 text-sm rounded-sm bg-green-500 text-white mt-4 hover:bg-white hover:border hover:border-green-900 hover:text-green-600 rounded-e-full rounded-s-full"
@@ -21,6 +28,7 @@
               <h3 v-show="logintag">Login</h3>
               <h3 v-show="!logintag">Loading...</h3>
             </button>
+            <button class="mt-3 font-montserrat mb-4 text-green-700 font-extrabold cursor-pointer" @click="showSignUp">Get Password</button>
           </div>
         </div>
       </form>
@@ -66,6 +74,7 @@ export default {
       this.logintag = false
   const login_data = {
     nse_number: this.username,
+    password: this.password,
   };
 
   try {
--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,117 +1,95 @@
 <template>
- <div class="flex justify-center pt-3 lg:pt-12 pb-72 " >
-        
-        <div class="bg-white p-2">
-          <div class="flex flex-col px-4 py-4 items-center w-86 lg:w-100 h-96 pt-14 border border-orange-900">
-            <label class="font-serif text-2xl mb-2 text-green-800">NSE-Member Signup</label>
-            <h3 class="text-green-800 font-pop text-sm" v-show="successSignup">Signup success</h3>
-            <h3 class="text-green-800 font-pop text-sm" v-show="successSignup">an administrator would confirm your credentials</h3>
-            <h3 class="text-green-800 font-pop text-sm" v-show="successSignup">Login access would be granted</h3>
-            <h3 class="text-green-800 font-pop text-sm" v-show="failedSignup">Credentials already used</h3>
-            <div
-            class="h-8 lg:w-64 rounded-sm sm:w-56 focus:outline-none text-xs focus:ring-10 font-pop focus:ring-green-950 ring-1 ring-green-800 mt-2"
-            >
-              <input
-                class="px-2 py-2 h-8 lg:w-64 w-64 rounded-sm sm:w-56 focus:outline-none text-xs focus:ring-10 font-pop focus:ring-green-800 ring-1 ring-green-400 "
-                type="email"
-                placeholder="Email :"
-                v-model="email"
-                required
-              />
-            </div>
-  
 
-            <div
-            class="h-8 lg:w-64 rounded-sm sm:w-56 focus:outline-none text-xs focus:ring-10 font-pop focus:ring-green-950 ring-1 ring-green-800 mt-2"
-            >
-              <input
-                class="px-2 py-2 h-8 lg:w-64 w-64 rounded-sm sm:w-56 focus:outline-none text-xs focus:ring-10 font-pop focus:ring-green-800 ring-1 ring-green-400 "
-                type="text"
-                placeholder="username :"
-                v-model="username"
-                required
-              />
-            </div>
-  
-            <div
-              class="flex justify-between text-base mt-4 border rounded-sm focus:ring-green-800 ring-green-400 ring-1 lg:w-64 w-64 sm:w-56"
-            >
-              <input
-                class="px-2 py-2 h-8 w-40 rounded-sm focus:outline-none text-xs focus:ring-10 font-pop border-0"
-                :type="inputType"
-                placeholder="password : "
-                v-model="password"
-                required
-              
-              />
-              <i
-                class="mt-2 mr-2 text-green-800"
-                :class="showPassword"
-                @click="togglePasswordVisibility"
-              ></i>
-            </div>
-            <button
-              class="font-pop border py-2 px-16 text-sm rounded-sm bg-green-500 text-white mt-4 hover:bg-white hover:border hover:border-green-900 hover:text-green-600 rounded-e-full rounded-s-full"
-              @click="signUp"
-            >
-              Signup
-            </button>
-            
-          <a class="mt-2 text-gray-600 text-xs" @click="showLoginForm">Have an account ? Login</a>
-  
-            
+  <div class="flex justify-center pt-3 lg:pt-12 pb-72 ">
+
+    <div class="bg-white p-2 rounded-md ">
+      <div class="flex flex-col px-4 py-4 items-center w-86 lg:w-100 h-96 pt-14 border border-orange-900">
+        <form>
+          <label class="font-montserrat font-extrabold text-lg mb-2 text-green-800 text-center">Get Password</label>
+          <h3 class="text-red-800 font-montserrat font-extrabold text-sm" v-show="failedSignup">Details Not Found</h3>
+          <div v-show="successSignup">
+            <h3 class="font-montserrat font-extrabold text-sm text-green-800 ">Check your email for password </h3>
           </div>
-        </div>
+
+          <div
+            class=" h-8 lg:w-64 sm:w-56 rounded-sm focus:outline-none text-xs focus:ring-10 font-pop focus:ring-green-950 ring-1 ring-green-800 mt-2"
+            v-show="!successSignup">
+
+            <input
+              class="px-2 py-2 h-8 lg:w-64 w-64 rounded-sm focus:outline-none sm:outline-none text-xs focus:ring-10 font-pop focus:ring-green-800 "
+              type="email" placeholder="EMAIL :" v-model="email" name="email" required />
+          </div>
+
+
+          <div
+            class="h-8 lg:w-64 rounded-sm sm:w-56 focus:outline-none text-xs focus:ring-10 font-pop focus:ring-green-950 ring-1 ring-green-800 mt-2"
+            v-show="!successSignup">
+            <input
+              class="px-2 py-2 h-8 lg:w-64 w-64 rounded-sm focus:outline-none text-xs focus:ring-10 font-pop focus:ring-green-800"
+              type="text" name="nse_no" placeholder="NSE-NO:" v-model="nse_no" required />
+          </div>
+
+
+          <button
+            class="font-pop border py-2 px-16 text-sm rounded-sm bg-green-500 text-white mt-4 hover:bg-white hover:border hover:border-green-900 hover:text-green-600 rounded-e-full rounded-s-full"
+            @click="signUp($event)" v-show="!successSignup">
+            Generate Password
+          </button>
+        </form>
+
+        <a class="mt-6 font-bold font-montserrat text-gray-600 text-xs cursor-pointer" @click="showLoginForm">
+        Gotten Password ? Login</a>
       </div>
+    </div>
+  </div>
 </template>
 <script>
 import axios from 'axios';
 
 
 export default {
-    name: "App",
-    data() {
-      return {
-        showPassword: "fas fa-eye",
-        inputType: "password",
-        username: "",
-        password: "",
-        showPasswordReq: false,
-        email : "",
-        showLogin : false,
-        successSignup : false,
-        failedSignup : false
+  name: "App",
+  data() {
+    return {
+      showPassword: "fas fa-eye",
+      inputType: "password",
+      username: "",
+      nse_no: "",
+      showPasswordReq: false,
+      email: "",
+      showLogin: false,
+      successSignup: false,
+      failedSignup: false
 
-      };
+    };
+  },
+  methods: {
+    showLoginForm() {
+      this.$emit('showLoginForm')
     },
-    methods : {
-        showLoginForm() { 
-            this.$emit('showLoginForm')
-        },
-        togglePasswordVisibility() { 
-          this.showPassword != !this.showPassword
-        },
-        async signUp(){
-          const SignupData = {
-            'username' : this.username,
-            'email' : this.email,
-            'password' : this.password,
-            'fullname' : '',
+    togglePasswordVisibility() {
+      this.showPassword != !this.showPassword
+    },
+    async signUp(event) {
+      event.preventDefault();
+      const SignupData = {
+        'email': this.email,
+        'nse_no': this.nse_no,
+      };
+      try {
+        const response = await axios.post('https://nse-backend-production.up.railway.app/api/signup', SignupData)
+        if (response.status === 200) {
+          this.successSignup = true
+        }
+        else {
+          this.failedSignup = true
+        }
+      } catch (error) {
+        this.failedSignup = true
+        console.log('error', error)
+      }
 
-          };
-          try {
-          const response = await axios.post('https://nse-backend-production.up.railway.app/api/signup', SignupData)
-          if(response.status === 200) {
-            this.successSignup = true
-          }
-          else { 
-            this.failedSignup = true
-          }
-        }catch(error) { 
-          console.log('error', error)
-        }
-          
-        }
     }
+  }
 }
 </script>
